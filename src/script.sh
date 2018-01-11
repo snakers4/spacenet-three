3207d0e249559175b37625092719ed5172d5b47f
snakers41 <snakers41@gmail.com>
2018-01-11 15:02:23 +0300

    Initial boilerplate

--- src/script.sh
+++ src/script.sh
@@ -1,15 +1,24 @@
 python3 train_satellites.py \
-	--arch linknet34 --batch-size 40 \
-	--imsize 320 --preset mul_ir1 --augs False \
-	--workers 6 --epochs 50 --start-epoch 0 \
-	--seed 42 --print-freq 20 \
-	--lr 1e-3 --optimizer adam \
-	--tensorboard True --tensorboard_images True --lognumber ln34_mulir1_noaug_dice\
-
-python3 train_satellites.py \
-	--arch unet11 --batch-size 20 \
+	--arch unet11 --batch-size 15 \
 	--imsize 320 --preset mul_ir1 --augs False \
 	--workers 6 --epochs 50 --start-epoch 0 \
 	--seed 42 --print-freq 20 \
 	--lr 1e-3 --optimizer adam \
 	--tensorboard True --tensorboard_images True --lognumber un11_mulir1_noaug_dice\
+
+# basic tests + augs
+python3 train_satellites.py \
+	--arch linknet34 --batch-size 40 \
+	--imsize 320 --preset mul_ir1 --augs True \
+	--workers 6 --epochs 40 --start-epoch 0 \
+	--seed 42 --print-freq 20 \
+	--lr 1e-3 --optimizer adam \
+	--tensorboard True --tensorboard_images True --lognumber ln34_mulir1_aug_dice\
+
+python3 train_satellites.py \
+	--arch unet11 --batch-size 15 \
+	--imsize 320 --preset mul_ir1 --augs True \
+	--workers 6 --epochs 40 --start-epoch 0 \
+	--seed 42 --print-freq 20 \
+	--lr 1e-3 --optimizer adam \
+	--tensorboard True --tensorboard_images True --lognumber un11_mulir1_aug_dice\
